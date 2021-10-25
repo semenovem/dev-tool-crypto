cbbc9d7e80ed6ac1e4c67d375e9a5222bc85a33e
semenov evgeny <emsemenov@vtb.ru>
2021-10-25 13:51:00 +0400

    fabric: change path inside start-ca file

--- fabric/start-ca.sh
+++ fabric/start-ca.sh
@@ -5,8 +5,8 @@ source "${BIN}/util.sh" ""
 
 # Проверить tls серты
 if [ ! -d "$__CONN_TLS__" ] || [ -z "$(ls "$__CONN_TLS__")" ]; then
-  bash "${BIN}/../conn-tls/conn-gen.sh"
+  bash "${BIN}/conn-tls/conn-gen.sh"
  fi
 
-bash "${BIN}/../cmd/tlsca-server.sh" || exit 1
-bash "${BIN}/../cmd/ca-server.sh" || exit 1
+bash "${BIN}/cmd/tlsca-server.sh" || exit 1
+bash "${BIN}/cmd/ca-server.sh" || exit 1
