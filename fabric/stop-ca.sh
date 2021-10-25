5f11b870117c3cc56c56adb6be12243694148eaa
semenov evgeny <emsemenov@vtb.ru>
2021-10-25 13:48:30 +0400

    fabric: move start-ca file

--- fabric/stop-ca.sh
+++ fabric/stop-ca.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BIN=$(dirname "$([[ $0 == /* ]] && echo "$0" || echo "$PWD/${0#./}")")
-source "${BIN}/../util.sh" ".."
+source "${BIN}/util.sh" ""
 
 PIDS=$(ps -fC fabric-ca-serve | awk '{print $2}' | tail -n +2)
 
