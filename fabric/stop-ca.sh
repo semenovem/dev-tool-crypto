803f4cd7848e1f7c08f8dbf7147d84756515c787
semenov evgeny <emsemenov@vtb.ru>
2022-03-01 18:14:03 +0300

    fix fix hard stop services fabric-ca-server

--- fabric/stop-ca.sh
+++ fabric/stop-ca.sh
@@ -4,7 +4,6 @@ BIN=$(dirname "$([[ $0 == /* ]] && echo "$0" || echo "$PWD/${0#./}")")
 source "${BIN}/util.sh" ""
 
 PIDS=$(ps -fC fabric-ca-serve | awk '{print $2}' | tail -n +2)
-
 for PID in $PIDS; do
   echo "stopping pid = $PID"
   kill -1 "$PID"
@@ -13,6 +12,7 @@ done
 sleep 3
 
 PIDS=$(ps -fC fabric-ca-serve | awk '{print $2}' | tail -n +2)
-if [ "$PIDS" ]; then
+for PID in $PIDS; do
+  echo "stopping pid = $PID"
   kill -9 "$PID"
-fi
+done
